7371c2006f4ab5138283fb2d5671a1d403083696
prabhjotaulakh12345 <prabhjotaulakh68@gmail.com>
2023-11-21 14:57:48 -0500

    Started UI for querying data

--- src/main/java/com/dealer/AdminApp.java
+++ src/main/java/com/dealer/AdminApp.java
@@ -1,12 +1,9 @@
 package com.dealer;
 
-import com.dealer.data.loaders.FileLoader;
 import com.dealer.data.loaders.IDataLoader;
 
 public class AdminApp {
     public static void main(String[] args) {
-        IDataLoader loader = new FileLoader();
-        Manager manager = new Manager(loader);
-        manager.runApp();
+        
     }
 }
--- src/main/java/com/dealer/Manager.java
+++ src/main/java/com/dealer/Manager.java
@@ -1,224 +1,104 @@
 package com.dealer;
 
-import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
-import com.dealer.data.exceptions.LoaderException;
+import com.dealer.data.filters.ICarFilter;
+import com.dealer.data.filters.ICustomerFilter;
+import com.dealer.data.filters.IEmployeeFilter;
 import com.dealer.data.loaders.IDataLoader;
 import com.dealer.data.sorters.AbstractCarSorter;
 import com.dealer.data.sorters.AbstractCustomerSorter;
 import com.dealer.data.sorters.AbstractEmployeeSorter;
-import com.dealer.data.sorters.Order;
-import com.dealer.data.sorters.impl.CarModelSorter;
-import com.dealer.data.sorters.impl.CarPriceSorter;
-import com.dealer.data.sorters.impl.CarYearSorter;
-import com.dealer.data.sorters.impl.CustomerNameSorter;
-import com.dealer.data.sorters.impl.EmployeeNameSorter;
-import com.dealer.data.sorters.impl.EmployeeSalarySorter;
-import com.dealer.models.cars.Car;
-import com.dealer.models.people.Customer;
-import com.dealer.models.people.Employee;
 
+/**
+ * Manager class for admins
+ * @author Prabhjot Aulakh, Safin Haque
+ */
 public class Manager {
-    private final int VIEW_CARS_OPTION = 1;
-    private final int VIEW_EMPLOYEES_OPTION = 2;
-    private final int VIEW_CUSTOMERS_OPTION = 3;
-    private final int DO_NOT_SORT = 4;
-    private final int SORT_CAR_BY_PRICE = 5;
-    private final int SORT_CAR_BY_MODEL = 6;
-    private final int SORT_CAR_BY_YEAR = 7;
-    private final int ASCENDING = 8;
-    private final int DESCENDING = 9;
-    private final int SORT_BY_NAME = 10;
-    private final int SORT_BY_SALARY = 11;
+    private IDataLoader dataLoader;
+    private AbstractCarSorter abstractCarSorter;
+    private AbstractCustomerSorter abstractCustomerSorter;
+    private AbstractEmployeeSorter abstractEmployeeSorter; 
+    private ICarFilter carFilter;
+    private ICustomerFilter customerFilter;
+    private IEmployeeFilter employeeFilter;
+    private Scanner scanner;
 
-    private Scanner sc;
-    private IDataLoader loader;
-    
-    public Manager(IDataLoader loader) {
-        this.sc = new Scanner(System.in);
-        this.loader = loader;
+    private final int CAR_OPTION = 1;
+    private final int CUSTOMER_OPTION = 2;
+    private final int EMPLOYEE_OPTION = 3;
+
+    /**
+     * Constructor
+     * @param dataLoader Strategy for loading data from a source (file/oracle)
+     */
+    public Manager(IDataLoader dataLoader) {
+        this.dataLoader = dataLoader;
+        this.abstractCarSorter = null;
+        this.abstractCustomerSorter = null;
+        this.abstractEmployeeSorter = null;
+        this.carFilter = null;
+        this.customerFilter = null;
+        this.employeeFilter = null;
+        this.scanner = new Scanner(System.in);
     }
 
-    public void runApp() {
-        System.out.println("Welcome to car management system !");
-        System.out.println("Please enter an a number to choose your next option");
-        while (true) {
-            this.displayOptions();
-            int option = chooseOption();
-            if (option == VIEW_CARS_OPTION) {
-                this.displayCars();
-            } else if (option == VIEW_EMPLOYEES_OPTION) {
-                this.displayEmployees();
-            } else if (option ==  VIEW_CUSTOMERS_OPTION) {
-                this.displayCustomers();
-            }
-        }
+    /**
+     * Sets the sorting strategy for cars
+     * @param sorter Strategy for sorting cars
+     */
+    public void setCarSortingStrategy(AbstractCarSorter sorter) { 
+        this.abstractCarSorter = sorter;
     }
 
-    private void displayOptions() {
-        System.out.println(VIEW_CARS_OPTION + ": View cars");
-        System.out.println(VIEW_EMPLOYEES_OPTION + ": View employees");
-        System.out.println(VIEW_CUSTOMERS_OPTION + ": View customers");
+    /**
+     * Sets the sorting strategy for customers
+     * @param sorter Strategy for sorting customers
+     */
+    public void setCustomerSortingStrategy(AbstractCustomerSorter sorter) { 
+        this.abstractCustomerSorter = sorter;
     }
 
-    private int chooseOption() {
-        while (true) {
-            try {
-                System.out.print("Option >>> ");
-                int option = this.sc.nextInt();
-                if (option != VIEW_CARS_OPTION && option != VIEW_EMPLOYEES_OPTION && option != VIEW_CUSTOMERS_OPTION) {
-                    System.out.println("You did not enter a valid option !");
-                } else {
-                    return option;
-                }
-            } catch (InputMismatchException e) { 
-                System.out.println("Please enter valid input");
-                this.sc.next();
-            }
-        }
+    /**
+     * Sets the sorting strategy for employees
+     * @param sorter Strategy for sorting employees
+     */
+    public void setEmployeeSortingStrategy(AbstractEmployeeSorter sorter) { 
+        this.abstractEmployeeSorter = sorter;
     }
 
-    private void displayCars() {
-        while (true) {
-            try {
-                List<Car> carList = this.loader.getCars();
-                System.out.println("Choose a sorting option: ");
-                System.out.println(DO_NOT_SORT + ": Skip sorting");
-                System.out.println(SORT_CAR_BY_PRICE + ": Sort by price");
-                System.out.println(SORT_CAR_BY_MODEL + ": Sort by model");
-                System.out.println(SORT_CAR_BY_YEAR + ": Sort by year");
-                int option = this.sc.nextInt();
-                if (    option != DO_NOT_SORT && 
-                        option != SORT_CAR_BY_PRICE && 
-                        option != SORT_CAR_BY_MODEL && 
-                        option != SORT_CAR_BY_YEAR) {
-                    System.out.println("Not a valid option");
-                } else if (option == DO_NOT_SORT) {
-                    printCars(carList);
-                    break;
-                }
-                Order order = askOrder();
-                AbstractCarSorter sorter = null;
-                if (option == SORT_CAR_BY_PRICE) {
-                    sorter = new CarPriceSorter(order);
-                } else if (option == SORT_CAR_BY_MODEL) {
-                    sorter = new CarModelSorter(order);
-                } else if (option == SORT_CAR_BY_YEAR) {
-                    sorter = new CarYearSorter(order);
-                } else {
-                    throw new IllegalArgumentException("Not a valid sorter");
-                }
-                sorter.sortCars(carList);
-                printCars(carList);
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter a number");
-            } catch (LoaderException e) {
-                e.printStackTrace();
-            }
-        }
+    /**
+     * Sets the filtering strategy for cars
+     * @param filter Strategy for filtering cars
+     */
+    public void setCarFilteringStrategy(ICarFilter filter) { 
+        this.carFilter = filter;
     }
 
-    private void printCars(List<Car> cars) {
-        for (Car car: cars) {
-            System.out.println(car);
-        }
+    /**
+     * Sets the filtering strategy for customers
+     * @param filter Strategy for filtering customers
+     */
+    public void setCustomerFilteringStrategy(ICustomerFilter filter) { 
+        this.customerFilter = filter;
     }
 
-    private void printEmployees(List<Employee> employees) {
-        for (Employee employee: employees) {
-            System.out.println(employee);
-        }
+    /**
+     * Sets the filtering strategy for employees
+     * @param filter Strategy for filtering employees
+     */
+    public void setEmployeeFilteringStrategy(IEmployeeFilter filter) { 
+        this.employeeFilter = filter;
     }
 
-    private void printCustomers(List<Customer> customers) {
-        for (Customer customer: customers) {
-            System.out.println(customer);
-        }
-    }
-
-    private Order askOrder() {
-        while (true) {
-            try {
-                System.out.println("Enter order for sorting");
-                System.out.println(ASCENDING + ": Ascending order");
-                System.out.println(DESCENDING + ": Descending order");
-                int order = this.sc.nextInt();
-                if (order != ASCENDING && order != DESCENDING) {
-                    System.out.println("Not a valid option");
-                } else if (order == ASCENDING) {
-                    return Order.ASCENDING;
-                } else {
-                    return Order.DESCENDING;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("You did not enter a valid value");
-                this.sc.next();
-            }
-        }
-    }
-
-    private void displayEmployees() {
-        while (true) {
-            try {
-                List<Employee> employees = this.loader.getEmployees();
-                System.out.println("Enter sorting option");
-                System.out.println(this.DO_NOT_SORT + ": Skip sorting");
-                System.out.println(this.SORT_BY_NAME + ": Sort by name");
-                System.out.println(this.SORT_BY_SALARY + ": Sort by salary");
-                int option = this.sc.nextInt();
-                if (option != DO_NOT_SORT && option != SORT_BY_SALARY && option != SORT_BY_NAME) {
-                    System.out.println("Please enter a valid option");
-                } else if (option == DO_NOT_SORT) {
-                    this.printEmployees(employees);   
-                }
-                Order order = this.askOrder();
-                AbstractEmployeeSorter sorter = null;
-                if (option == SORT_BY_NAME) {
-                    sorter = new EmployeeNameSorter(order);
-                } else if (option == SORT_BY_SALARY) {
-                    sorter = new EmployeeSalarySorter(order);
-                }
-                sorter.sortEmployees(employees);
-                this.printEmployees(employees);
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter a valid option");
-                this.sc.next();
-            } catch (LoaderException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void displayCustomers() {
-        while (true) {
-            try {
-                List<Customer> customers = this.loader.getCustomers();
-                System.out.println("Enter sorting option");
-                System.out.println(this.DO_NOT_SORT + ": Skip sorting");
-                System.out.println(this.SORT_BY_NAME + ": Sort by name");
-                int option = this.sc.nextInt();
-                if (option != DO_NOT_SORT && option!= SORT_BY_NAME) {
-                    System.out.println("Please enter a valid option");
-                } else if (option == DO_NOT_SORT) {
-                    this.printCustomers(customers);   
-                    break;
-                } else {
-                    Order order = this.askOrder();
-                    AbstractCustomerSorter sorter = new CustomerNameSorter(order);
-                    sorter.sortCustomers(customers);
-                    this.printCustomers(customers);
-                    break;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter valid input");
-                this.sc.next();
-            } catch (LoaderException e) {
-                e.printStackTrace();
-            }
-        }
+    /**
+     * Entry point of the manager class
+     */
+    public void run() {
+        System.out.println("Welcome to car inventory manager!");
+        System.out.println("Please choose an option from below: ");
+        System.out.println(this.CAR_OPTION + ": view  cars");
+        System.out.println(this.CUSTOMER_OPTION + ": view customers");
+        System.out.println(this.EMPLOYEE_OPTION + ": view employees");
     }
 }
