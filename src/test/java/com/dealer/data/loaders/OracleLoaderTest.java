746dd0295cd8f91122d879ffc531b23d072e9b0b
Prabhjot Aulakh <prabhjot.aulakh@dawsoncollege.qc.ca>
2023-11-22 22:37:42 -0500

    Variable naming for OracleLoaderTest.java

--- src/test/java/com/dealer/data/loaders/OracleLoaderTest.java
+++ src/test/java/com/dealer/data/loaders/OracleLoaderTest.java
@@ -15,24 +15,24 @@ import com.dealer.models.cars.RecreationalVehicle;
 public class OracleLoaderTest {
     @Test
     public void return_list_of_12_cars() throws LoaderException {
-        IDataLoader fileLoader = new OracleLoader();
-        List<Car> list = fileLoader.getCars();
+        IDataLoader oracleLoader = new OracleLoader();
+        List<Car> list = oracleLoader.getCars();
         assertEquals(12, list.size());
     }
 
     @Test
     public void first_car_matches() throws LoaderException {
         Car other = new Car("Toyota Corolla", 2005, "Red", 8000);
-        IDataLoader fileLoader = new OracleLoader();
-        List<Car> list = fileLoader.getCars();
+        IDataLoader oracleLoader = new OracleLoader();
+        List<Car> list = oracleLoader.getCars();
         assertTrue(list.get(0).equals(other));
     }
 
     @Test
     public void first_electric_car_matches() throws LoaderException {
         ElectricCar other = new ElectricCar("Tesla Model 3", 2020, "Blue", 45000, 480, "Supercharger");
-        IDataLoader fileLoader = new OracleLoader();
-        List<Car> list = fileLoader.getCars();
+        IDataLoader oracleLoader = new OracleLoader();
+        List<Car> list = oracleLoader.getCars();
         System.out.println(list.get(0).getModel());
         assertTrue(list.get(6).equals(other));
     }
@@ -40,8 +40,8 @@ public class OracleLoaderTest {
     @Test
     public void first_rv_matches() throws LoaderException {
         RecreationalVehicle other = new RecreationalVehicle("Airstream Basecamp", 2021, "Silver", 40000, 4, 2, false);
-        IDataLoader fileLoader = new OracleLoader();
-        List<Car> list = fileLoader.getCars();
+        IDataLoader oracleLoader = new OracleLoader();
+        List<Car> list = oracleLoader.getCars();
         assertTrue(list.get(10).equals(other));
     }
 }
